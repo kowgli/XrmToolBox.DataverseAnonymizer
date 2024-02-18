f97d4ed6eb62c7d8544ac5d3918e9ee7668b4688
Lukasz Grzybowski-Glikman <lukasz@crmdev365.com>
2024-02-18 18:49:28 +0100

    Assembly and package rename

--- XrmToolBox.DataverseAnonymizer/Properties/AssemblyInfo.cs
+++ XrmToolBox.DataverseAnonymizer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XrmToolBox.DataverseAnonymizer")]
+[assembly: AssemblyTitle("CrmDev365.DataverseAnonymizer")]
 [assembly: AssemblyDescription("XrmToolBox tool for anonymizing data in the Dataverse")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CrmDev365")]
-[assembly: AssemblyProduct("XrmToolBox.DataverseAnonymizer")]
+[assembly: AssemblyProduct("CrmDev365.DataverseAnonymizer")]
 [assembly: AssemblyCopyright("Copyright CrmDev365© 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
